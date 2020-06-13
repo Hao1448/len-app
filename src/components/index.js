7cb7817f3c25a7e57dcac42b74068c614b958237
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-13 14:41:05 +0300

    add button to components

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
 import Header from './Header'
 import Grid from './Grid'
 import Container from './Container'
-import Button from './Button'
+import UiButton from './Button'
 
 // export * from './Cards'
 // export * from './Widgets'
 
-export { Header, Grid, Container, Button }
+export { Header, Grid, Container, UiButton }
