a1225b28e685078fa33c6a969d2b469234c4642f
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-13 14:12:10 +0300

    Create theme.js

---
+++ src/base/theme.js
@@ -0,0 +1,9 @@
+export const GlobalTheme = {
+    color: {
+        black: '#555555',
+        white: '#FFFFFF',
+        grey: '#DDDDDD',
+        red: '#D24A43',
+        grey_light: '#EEEEEE'
+    }
+}
