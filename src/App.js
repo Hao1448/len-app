c3d566deed1900a7f3214b023e86acf5ec42fa69
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-13 14:11:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import { ThemeProvider } from 'styled-components'
+import { GlobalTheme } from './base/theme'
+import { Header } from './components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={GlobalTheme}>
+      <Wrapper>
+        <Header/>
+      </Wrapper>
+    </ThemeProvider>
   );
 }
+
+const Wrapper = styled.div` 
+  padding-top: 20px;
+`
 
 export default App;
