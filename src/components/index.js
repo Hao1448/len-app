4e9a055a0ae2a84d8bcd44aa55c4eaf37800c847
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-13 18:00:36 +0300

    Update components

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,8 @@ import Header from './Header'
 import Grid from './Grid'
 import Container from './Container'
 import UiButton from './Button'
+import CardReview from './CardReview'
 
-// export * from './Cards'
 export * from './Widgets'
 
-export { Header, Grid, Container, UiButton }
+export { Header, Grid, Container, UiButton, CardReview }
