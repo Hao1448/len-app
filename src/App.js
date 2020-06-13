a9414e95f990a33f78a2673f0ff254b751e2cb48
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-13 19:28:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React from 'react';
 import styled, { ThemeProvider} from 'styled-components'
 import { GlobalTheme } from './base/theme'
 import { GlobalStyle } from './base/styles'
-import { Header, WidgetHero, WidgetReview } from './components'
+import { Header, WidgetHero, WidgetReview, WidgetSchedule, Footer } from './components'
 
 function App() {
   return (
     <ThemeProvider theme={GlobalTheme}>
       <GlobalStyle/>
       <Wrapper>
-        <Header/>
-        <WidgetHero/>
-        <WidgetReview/>
+        <Row>
+          <Header/>
+        </Row>
+        <Row>
+          <WidgetHero/>
+        </Row>
+        <Row>
+          <WidgetReview/>
+        </Row>
+        <Row>
+          <WidgetSchedule/>
+        </Row>
+        <Row>
+          <Footer/>
+        </Row>
       </Wrapper>
     </ThemeProvider>
   );
@@ -20,5 +32,9 @@ function App() {
 const Wrapper = styled.div` 
   padding-top: 20px;
 `
-
+const Row = styled.div`
+  & + & {
+    margin-top: 40px;
+  }
+`
 export default App;
